aec6fc8d5c4436bea0d3c6fd2cdc6338141a3963
v-king777 <v.kolpashchikov@gmail.com>
2021-10-20 21:58:49 +0300

    Добавьте файлы проекта.

---
+++ UserBlogs/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserBlogs.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        ///  Middleware-компонент должен иметь конструктор, принимающий RequestDelegate
+        /// </summary>
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        ///  Необходимо реализовать метод Invoke или InvokeAsync
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            LogConsole(context);
+            await LogFile(context);
+            await _next.Invoke(context);
+        }
+
+        /// <summary>
+        /// Логируем в консль
+        /// </summary>
+        private void LogConsole(HttpContext context)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: New request to http://" +
+                $"{context.Request.Host.Value + context.Request.Path}");
+        }
+
+        /// <summary>
+        /// Логиуем в файл
+        /// </summary>
+        private async Task LogFile(HttpContext context)
+        {
+            string logMessage = $"[{DateTime.Now}]: New request to http://" +
+                $"{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+
+            string logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "RequestLog.txt");
+
+            await File.AppendAllTextAsync(logFilePath, logMessage);
+        }
+    }
+}
